dbbb67c87bb5398e8ee38398aff7dd7481a7f2bd
John Denver <acar.johndenver@gmail.com>
2025-04-24 00:40:59 +0800

    Working on grade tracker

--- dict_practice.py
+++ dict_practice.py
@@ -7,53 +7,138 @@ def get_user_operation():
                         "============================ \n"
                         "1. Add a student and their grade \n"
                         "2. View all student and their grade \n"
-                        "3. Update a grade \n"
-                        "4. Delete a student \n"
-                        "5. Show class average \n"
-                        "6. Show student average \n"
-                        "7. Exit \n"
+                        "3. View student's specific grade \n"
+                        "4. View student's GWA\n"
+                        "5. Update a grade \n"
+                        "6. Delete a student \n"
+                        "7. Show class average \n"
+                        "8. Show student average \n"
+                        "9. Exit \n"
                         "Input: "))
+            print()
+            if choices < 1 or choices > 9:
+                print("Please enter number 1-7 only!")
+                print()
+            else:
+                is_user_input_valid = True
+                return choices
 
         except ValueError:
             print("Please enter number only!")
             print()
 
-        if choices < 1 or choices > 7:
-            print("Please enter number 1-7 only!")
-            print()
-        else:
-            is_user_input_valid = True
-            return choices
-
-def add_student_and_grade():
-    name = input("What is the student's name? ")
-    grade = float(input("What is the student's grade? "))
-
-    student_info = {
-        "name": name,
-        "grade": grade
-    }
-    students.append(student_info)
-
 def user_continue():
-    try:
+    is_user_continue = True
+    while is_user_continue:
         cont = input("Do you still want to continue? ").upper()
         if cont == "YES":
             print()
             return True
-        else:
+        elif cont == "NO":
             print("Thank You!")
             return False
-    except ValueError:
-        print("Please Enter YES or NO only!")
+        else:
+            print("Please Enter YES or NO only!")
+
+def add_student_and_grade():
+    name = input("What is the student's name? ")
+    while True:
+        try:
+            math_grade = float(input("What is the student's grade in math subject? "))
+            science_grade = float(input("What is the student's grade in science subject? "))
+            english_grade = float(input("What is the student's grade in english subject? " ))
+            student_info = {
+                "name": name,
+                "math": math_grade,
+                "science": science_grade,
+                "english": english_grade
+            }
+            students.append(student_info)
+            break
+        except ValueError:
+            print("Please Enter Number Only!")
+            print()
+
+def view_all_students_grade():
+    for i in range(len(students)):
+        print(f"Student {i + 1}'s info:")
+        print(f"Name: {students[i].get("name")}")
+        print(f"Math Grade: {students[i].get("math")}")
+        print(f"Science Grade: {students[i].get("science")}")
+        print(f"English Grade: {students[i].get("english")}")
+        print()
+
+def view_student_specific_grade():
+    find_name = input("Please enter student's name you want to view: ").lower()
+    find_subject = input("Please enter the subject you want to view: ").lower()
+
+    found = False
+    for student in students:
+        if student["name"].lower() == find_name:
+            if find_subject in student:
+                print(f"{student['name']}'s grade in {find_subject} is: {student[find_subject]}")
+                found = True
+                break
+    if not found:
+        print("Student or subject is nout found")
+
+def student_gwa():
+    found = False
+    gwa_name = input("Which student's GWA you want to view? ")
+    for student in students:
+        if student["name"] == gwa_name:
+            gwa = (student["math"] + student["science"] + student["english"]) / 3
+            print(f"{student["name"]}'s GWA is: {gwa}")
+            found = True
+            break
+    if not found:
+        print("Student not found")
+
+def update_student_grade():
+    name_update = input("Which student's grade you want to update? ")
+    subject_update = input("Which subject you want to update? ")
+    is_input_valid = False
+    student_found = False
+
+    while not is_input_valid:
+        try:
+            updated_grade = float(input("Updated Grade: "))
+            is_input_valid = True
+        except ValueError:
+            print("Please enter number only!")
+
+    for student in students:
+        if student["name"].lower() == name_update.lower():
+            student_found = True
+            if subject_update in student:
+                student[subject_update] = updated_grade
+                subject_found = True
+                print("Grade updated successfully!")
+                break
+            else:
+                print("Subject not found!")
+
+    if not student_found:
+        print("Student not found!")
 
 students = []
 
-while True:
-    if get_user_operation() == 1:
+user_continuation = True
+while user_continuation:
+    operation = get_user_operation()
+    if operation == 1:
         add_student_and_grade()
+    elif operation == 2:
+        view_all_students_grade()
+    elif operation == 3:
+        view_student_specific_grade()
+    elif operation == 4:
+        student_gwa()
+    elif operation == 5:
+        update_student_grade()
+    user_continuation = user_continue()
 
-for i in range(len(students)):
-    print("Student " + str(int((i + 1))) + "'s name is: " + students[i].get("name"))
-    print("Student " + str(int((i + 1))) + "'s grade is: " + str(students[i].get("grade")))
-    print()
+
+
+
+
--- try-except.py
+++ try-except.py
@@ -1,83 +1,22 @@
-# def is_count_valid():
-#     try:
-#         count = int(input("Enter How Many Numbers You Want to Add: "))
-#         if count > 0:
-#             return count
-#         else:
-#             print("Please Input Number greater than 0")
-#             return None
-#     except ValueError:
-#         print("Please Input Numbers Only")
-#         return None
-#
-# def is_number_valid(prompt):
-#     valid = False
-#     while not valid:
-#         try:
-#             number = int(input(prompt))
-#             return number
-#         except ValueError:
-#             print("Please Enter Numbers Only!")
-#
-# count = None
-# while count is None:
-#     count = is_count_valid()
-#     for i in range(count):
-#         inputs = is_number_valid(f"Enter Number {i + 1}: ")
-#
+students = []
 
-def cont_calculating():
-    check = True
-    while check:
-        con = input("Do You Still Want To Cotinue? ").upper()
-        if con == "YES":
-            return True
-        elif con == "NO":
-            print("Thank You!")
-            return False
-        else:
-            print("Please Enter YES or NO only!")
+name = input("What is the student's name? ")
+while True:
+    try:
+        math_grade = float(input("What is the student's grade in math subject? "))
+        science_grade = float(input("What is the student's grade in science subject? "))
+        english_grade = float(input("What is the student's grade in english subject?" ))
+        student_info = {
+            "name": name,
+            "math": math_grade,
+            "science": science_grade,
+            "english": english_grade
+        }
+        students.append(student_info)
+        break
+    except ValueError:
+        print("Please Enter Number Only!")
+        print()
 
-continue_calculating = True
-while continue_calculating:
-    continue_calculating = cont_calculating()
-
-    #
-    # elif op == 2:
-    #     count = int(input("Enter How Many Number You Want to Subtract: "))
-    #     inputs = [float(input(f"Enter Number {i + 1}: ")) for i in range(count)]
-    #
-    #     if count or inputs == "":
-    #         inputs = [float(input("Please Enter a Valid Number: ")) for i in range(count)]
-    #         result = sub(*inputs)
-    #         print("The Result is:", result)
-    #     else:
-    #         result = sub(*inputs)
-    #         print("The Result is:", result)
-    #
-    # elif op == 3:
-    #     count = int(input("Enter How Many Number You Want to Add: "))
-    #     inputs = [float(input(f"Enter Number {i + 1}: ")) for i in range(count)]
-    #
-    #     if count and inputs == "":
-    #         inputs = [float(input("Please Enter a Valid Number: ")) for i in range(count)]
-    #         result = mult(*inputs)
-    #         print("The Result is:", result)
-    #     else:
-    #         result = mult(*inputs)
-    #         print("The Result is:", result)
-    #
-    # elif op == 4:
-    #     numerator = float(input("Enter the Numerator: "))
-    #     denominator = float(input("Enter the Denominator: "))
-    #
-    #     if numerator and denominator == "":
-    #         numerator = float(input("Please Input Valid Numerator: "))
-    #         denominator = float(input("Please Input Valid Denominator: "))
-    #         if denominator != 0:
-    #             result = div(numerator, denominator)
-    #             print("The Result is: ", result)
-    #         else:
-    #             print("0 as Denominator is invalid")
-    #     result = div(numerator, denominator)
-    #     print("The Result is: ", result)
+for student in students:
+    print(student["name"])
